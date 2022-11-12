363c1e1fe19d0ac25b396d763477a4f866e202aa
Willy Gutsche <willy.gutsche@unifesp.br>
2022-11-11 23:23:20 -0300

    Implementação dos Structs

---
+++ Matriz esparsa/main.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+typedef struct no {
+    int valor;
+    int coluna;
+    struct no *proximo;
+}No;
+
+typedef struct {
+    No **matriz;
+    int linhas;
+    int colunas;
+}Matriz;
+
+
+
+
+
+
+
+int main() {
+
+
+
+
+
+
+
+
+
+    return 0;
+}
