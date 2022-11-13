326d5c428a40cffbf7b9b02b0eeb244d2a048ed1
Willy Gutsche <willy.gutsche@unifesp.br>
2022-11-12 23:51:12 -0300

    Função: inserirMatriz

--- Matriz esparsa/main.c
+++ Matriz esparsa/main.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <stdbool.h>
 
 typedef struct no {
     int valor;
@@ -26,7 +27,48 @@ Matriz* inicializarMatriz(int linhas, int colunas) {
     return mat;
 }
 
+bool inserirMatriz(Matriz *mat, int linha, int coluna, int valor) {
+    // verificando se a linha e a coluna são válidas  
+    if (linha < 0 || linha >= mat->linhas || coluna < 0 || coluna >= mat->colunas) {
+        return false;
+    }
 
+    // busca pela posição da linha e da coluna
+    No *anterior = NULL;
+    No *atual = mat->matriz[linha];
+    while (atual != NULL && atual->coluna < coluna) {
+        anterior = atual;
+        atual = atual->proximo;
+    }
+
+    if (atual != NULL && atual->coluna == coluna) {
+        // modificando o valor da posição se o Nó dela já existir
+        if (valor == 0) {
+            if (anterior == NULL) {
+                mat->matriz[linha] = atual->proximo;
+            } else {
+                anterior->proximo = atual->proximo;
+            }
+            free(atual);
+        } else {
+            atual->valor = valor;
+        }
+    } else if (valor != 0) {
+        // criando Nó e inserindo na matriz
+        No *novo = (No*) malloc(sizeof(No));
+
+        novo->valor = valor;
+        novo->coluna = coluna;
+        novo->proximo = atual;
+        if (anterior == NULL) {
+            mat->matriz[linha] = novo;
+        } else {
+            anterior->proximo = novo;
+        }
+    }
+
+    return true;
+}
 
 
 
