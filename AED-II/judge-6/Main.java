9ddf139103737192ae97ec88dd7fa7fb00927833
Willy Gutsche <willy.gutsche@unifesp.br>
2023-05-03 19:28:43 -0300

    Radix-Sort and Counting-Sort

--- AED-II/judge-6/Main.java
+++ AED-II/judge-6/Main.java
@@ -1,40 +1,129 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        final List<Integer> values = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 13, 0, 0, 1, 0));
+        final int wordsQuantity = scanner.nextInt();
+        scanner.nextLine();
 
-        final List<Integer> sortedValues = new CountingSort(values).countingSort();
+        final Map<Character, Integer> characterPriorityKeys = inputCharacterPriorityKeys();
 
-        System.out.println(sortedValues);
+        final List<String> words = inputWords();
+        printWordsInitial(words);
+
+        final int maxLength = maxLength(words);
+        matchWordsLength(words, maxLength);
+        System.out.println(maxLength);
+
+        final List<String> sortedWords = new RadixSort(words, characterPriorityKeys, maxLength).radixSort();
+        printSortedWords(sortedWords);
+    }
+
+    private static Map<Character, Integer> inputCharacterPriorityKeys() {
+        final String priorityKeys = scanner.nextLine();
+        final Map<Character, Integer> characterPriorityKeys = new HashMap<>() { { put(' ', 0); } };
+
+        int priority = 1;
+
+        for (Character key : priorityKeys.toCharArray()) {
+            characterPriorityKeys.put(key, priority);
+            priority++;
+        }
+        return characterPriorityKeys;
+    }
+
+    private static List<String> inputWords() {
+        final String words = scanner.nextLine();
+        final String lowerCaseWords = words.toLowerCase();
+        final String[] splitWords = lowerCaseWords.split(" ");
+        return Arrays.asList(splitWords);
+    }
+
+    private static void matchWordsLength(List<String> words, int maxLength) {
+        for (int i = 0; i < words.size(); i++) {
+            final String word = words.get(i);
+            final int wordLength = word.length();
+
+            if (wordLength < maxLength) {
+                final int gapLength = maxLength - wordLength;
+                final String spaces = " ".repeat(gapLength);
+
+                words.set(i, word.concat(spaces));
+            }
+        }
+    }
+
+    private static int maxLength(List<String> words) {
+        return words.stream()
+                .map(String::length)
+                .max(Comparator.naturalOrder())
+                .get();
+    }
+
+    private static void printWordsInitial(List<String> words) {
+        words.forEach( word -> {
+            System.out.println(word.concat("."));
+        });
+    }
+
+    private static void printSortedWords(List<String> words) {
+        System.out.print(words.get(0));
+
+        for (int i = 1; i < words.size(); i++) {
+            System.out.print("\n" + words.get(i));
+        }
     }
 }
 
-class CountingSort {
-    private final List<Integer> values;
+class RadixSort {
+    private List<String> words;
+    private final Map<Character, Integer> characterPriorityKeys;
+    private final Integer maxWordLength;
 
-    public CountingSort(List<Integer> values) {
-        this.values = values;
+    public RadixSort(List<String> words, Map<Character, Integer> characterPriorityKeys, Integer maxWordLength) {
+        this.words = words;
+        this.characterPriorityKeys = characterPriorityKeys;
+        this.maxWordLength = maxWordLength;
     }
 
-    public List<Integer> countingSort() {
-        final List<Integer> auxiliaryList = createAuxiliaryList();
-        final Integer[] sortedValues = new Integer[values.size()];
+    public List<String> radixSort() {
+        for (int i = maxWordLength - 1; i >= 0; i--) {
+            List<Integer> columnValues = convertCharacterColumnToValues(i);
+            countingSort(columnValues);
+        }
+        return words;
+    }
+
+    private List<Integer> convertCharacterColumnToValues(int columnIndex) {
+        return words.stream()
+                .map(word -> word.charAt(columnIndex))
+                .map(characterPriorityKeys::get)
+                .collect(Collectors.toList());
+    }
+
+    private void countingSort(List<Integer> values) {
+        final List<Integer> auxiliaryList = createAuxiliaryList(values);
+        printAuxiliaryList(auxiliaryList);
+
+        final String[] sortedWords = new String[values.size()];
 
         for (int i = values.size() - 1; i >= 0; i--) {
             final int mainValue = values.get(i);
             final int auxiliaryValue = auxiliaryList.get(mainValue);
 
-            sortedValues[auxiliaryValue - 1] = mainValue;
+            final String currentWord = words.get(i);
+            sortedWords[auxiliaryValue - 1] = currentWord;
+
             decreaseIndexFrequency(auxiliaryList, mainValue);
         }
-        return Arrays.asList(sortedValues);
+
+        words = Arrays.asList(sortedWords);
     }
 
-    private List<Integer> createAuxiliaryList() {
-        final int size = getMaximumValue();
+    private List<Integer> createAuxiliaryList(List<Integer> values) {
+        final int size = 27;
         final int defaultValue = 0;
         final List<Integer> auxiliaryList = new ArrayList<>();
 
@@ -42,21 +131,22 @@ class CountingSort {
             auxiliaryList.add(i, defaultValue);
         }
 
-        increaseFrequencyForEachValue(auxiliaryList);
+        increaseFrequencyForEachValue(values, auxiliaryList);
         sumPreviousValues(auxiliaryList);
 
         return auxiliaryList;
     }
 
-    private Integer getMaximumValue() {
-        Integer maximumValue = values.stream()
-                .max(Comparator.naturalOrder())
-                .orElse(-1);
+    private void printAuxiliaryList(List<Integer> auxiliaryList) {
+        System.out.print(auxiliaryList.get(0));
 
-        return maximumValue + 1;
+        for (int i = 1; i < auxiliaryList.size(); i++) {
+            System.out.print(" " + auxiliaryList.get(i));
+        }
+        System.out.println();
     }
 
-    private void increaseFrequencyForEachValue(List<Integer> auxiliaryList) {
+    private void increaseFrequencyForEachValue(List<Integer> values, List<Integer> auxiliaryList) {
         values.forEach( value -> {
                 final int currentFrequency = auxiliaryList.get(value);
                 auxiliaryList.set(value, currentFrequency + 1);
