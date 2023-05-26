1ca5a50a9102cc79ea4f7eddaa6b203333fbac2b
Willy Gutsche <willy.gutsche@unifesp.br>
2023-05-25 23:01:42 -0300

    Last features

--- AED-II/judge-8/Main.java
+++ AED-II/judge-8/Main.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final List<Integer> keys = inputKeys();
-        RedBlackTree redBlackTree = new RedBlackTree(keys);
-        redBlackTree.print();
+
+        AVLTree avlTree = new AVLTree(keys);
+        avlTree.printHeights();
         System.out.println();
+
+        RedBlackTree redBlackTree = new RedBlackTree(keys);
         RedBlackNode.printHeight(redBlackTree.root);
         System.out.println();
-        System.out.println(redBlackTree.colorChanges);
+
+        System.out.println(redBlackTree.blackHeight());
+
+        System.out.println(redBlackTree.colorChanges + ", " + redBlackTree.rotations + ", " + avlTree.rotations);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -243,5 +249,182 @@ class RedBlackTree {
             node = node.left;
         }
         return blackHeight;
+    }
+}
+
+class Node {
+    private Integer key;
+    private Node leftNode;
+    private Node rigthNode;
+    private Integer height;
+
+    public Node(Integer key) {
+        this.key = key;
+        this.leftNode = null;
+        this.rigthNode = null;
+        this.height = 0;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public Node getRigthNode() {
+        return rigthNode;
+    }
+
+    public static Integer getHeight(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    public static Integer getBalance(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.getLeftNode()) - getHeight(node.getRigthNode());
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public void setLeftNode(Node node) {
+        leftNode = node;
+    }
+
+    public void setRightNode(Node node) {
+        rigthNode = node;
+    }
+
+    public void setHeight() {
+        final Integer leftHeight = getHeight(leftNode);
+        final Integer rightHeight = getHeight(rigthNode);
+
+        if (leftHeight >= rightHeight) {
+            this.height = leftHeight + 1;
+            return;
+        }
+        this.height = rightHeight + 1;
+    }
+}
+
+class AVLTree {
+    private Node firstNode;
+    public Integer rotations;
+
+    public AVLTree(List<Integer> keys) {
+        rotations = 0;
+        createAVLTree(keys);
+    }
+
+    private void setFirstNode(Node node) {
+        this.firstNode = node;
+    }
+
+    private void createAVLTree(List<Integer> keys) {
+        keys.forEach( this::add );
+    }
+
+    private Node addNode(Integer key, Node referenceNode) {
+        if (referenceNode == null) {
+            return new Node(key);
+        }
+
+        if (key < referenceNode.getKey()) {
+            referenceNode.setLeftNode(addNode(key, referenceNode.getLeftNode()));
+        } else if (key > referenceNode.getKey()) {
+            referenceNode.setRightNode(addNode(key, referenceNode.getRigthNode()));
+        }
+
+        referenceNode.setHeight();
+        return balanceNode(referenceNode);
+    }
+
+    public void add(Integer key) {
+        final Node firstNodeAdded = addNode(key, firstNode);
+        setFirstNode(firstNodeAdded);
+    }
+
+    private Node leftRotate(Node node) {
+        final Node auxiliaryNode = node.getRigthNode();
+        final Node auxiliaryLeft = auxiliaryNode.getLeftNode();
+
+        node.setRightNode(auxiliaryLeft);
+        auxiliaryNode.setLeftNode(node);
+
+        node.setHeight();
+        auxiliaryNode.setHeight();
+
+        return auxiliaryNode;
+    }
+
+    private Node rightRotate(Node node) {
+        final Node auxiliaryNode = node.getLeftNode();
+        final Node auxiliaryRight = auxiliaryNode.getRigthNode();
+
+        node.setLeftNode(auxiliaryRight);
+        auxiliaryNode.setRightNode(node);
+
+        node.setHeight();
+        auxiliaryNode.setHeight();
+
+        return auxiliaryNode;
+    }
+
+    private Node doubleLeftRotate(Node node) {
+        node.setRightNode(rightRotate(node.getRigthNode()));
+        return leftRotate(node);
+    }
+
+    private Node doubleRightRotate(Node node) {
+        node.setLeftNode(leftRotate(node.getLeftNode()));
+        return rightRotate(node);
+    }
+
+    private Node balanceNode(Node node) {
+        final int nodeBalance = Node.getBalance(node);
+        final int leftBalance = Node.getBalance(node.getLeftNode());
+        final int rightBalance = Node.getBalance(node.getRigthNode());
+
+        if (nodeBalance < -1 && rightBalance <= 0) {
+            rotations++;
+            return leftRotate(node);
+        }
+
+        if (nodeBalance > 1 && leftBalance >= 0) {
+            rotations++;
+            return rightRotate(node);
+        }
+
+        if (nodeBalance > 1) {
+            rotations++;
+            return doubleRightRotate(node);
+        }
+
+        if (nodeBalance < -1) {
+            rotations++;
+            return doubleLeftRotate(node);
+        }
+
+        return node;
+    }
+
+    public void printHeights() {
+        printHeight(firstNode);
+    }
+
+    public void printHeight(Node node) {
+        final Integer firstNodeHeight = Node.getHeight(node);
+        final Integer leftHeight = Node.getHeight(node.getLeftNode()) + 1;
+        final Integer rightHeight = Node.getHeight(node.getRigthNode()) + 1;
+
+        System.out.print(firstNodeHeight + ", " + leftHeight + ", " + rightHeight);
     }
 }
