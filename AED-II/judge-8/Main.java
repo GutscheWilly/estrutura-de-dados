0cf8f9555359abed0b532d61c9108533a88e2baf
Willy Gutsche <willy.gutsche@unifesp.br>
2023-05-25 22:20:18 -0300

    Heigth feature

--- AED-II/judge-8/Main.java
+++ AED-II/judge-8/Main.java
@@ -7,6 +7,8 @@ public class Main {
         final List<Integer> keys = inputKeys();
         RedBlackTree redBlackTree = new RedBlackTree(keys);
         redBlackTree.print();
+        System.out.println();
+        RedBlackNode.printHeight(redBlackTree.root);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -23,43 +25,74 @@ public class Main {
     }
 }
 
-class Node {
+class RedBlackNode {
     public Integer key;
-    public Node father, left, right;
+    public RedBlackNode father, left, right;
     public boolean isRed;
+    public Integer height;
 
-    public Node(int key, boolean isRed) {
+    public RedBlackNode(int key, boolean isRed) {
         this.key = key;
         this.isRed = isRed;
         this.father = this.left = this.right = RedBlackTree.nil;
+        this.height = 0;
     }
 
-    public Node findNode(int key) {
+    public RedBlackNode findNode(int key) {
         if (key < this.key && this.left != RedBlackTree.nil) {
             return this.left.findNode(key);
         }
+
         if (key > this.key && this.right != RedBlackTree.nil) {
             return this.right.findNode(key);
         }
         return this;
     }
+
+    private Integer calculateHeight(RedBlackNode node) {
+        if (node == null) {
+            return -1;
+        }
+
+        if (node.left == RedBlackTree.nil && node.right == RedBlackTree.nil) {
+            return 0;
+        }
+
+        final int leftHeight = calculateHeight(node.left);
+        final int rightHeight = calculateHeight(node.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public void updateHeight() {
+        this.height = calculateHeight(this);
+    }
+
+    public static void printHeight(RedBlackNode node) {
+        final Integer firstNodeHeight = node.height;
+        final Integer leftHeight = node.left.height + 1;
+        final Integer rightHeight = node.right.height + 1;
+
+        System.out.print(firstNodeHeight + ", " + leftHeight + ", " + rightHeight);
+    }
 }
 
 class RedBlackTree {
-    public Node root;
-    public static Node nil = new Node(-1, false);
+    public RedBlackNode root;
+    public static RedBlackNode nil = new RedBlackNode(-1, false);
 
     public RedBlackTree(List<Integer> keys) {
         this.root = RedBlackTree.nil;
         createTree(keys);
+        updateHeights();
     }
 
     private void createTree(List<Integer> keys) {
         keys.forEach(this::add);
     }
 
-    private void leftRotate(Node x) {
-        final Node y = x.right;
+    private void leftRotate(RedBlackNode x) {
+        final RedBlackNode y = x.right;
         x.right = y.left;
 
         if (y.left != RedBlackTree.nil) {
@@ -80,8 +113,8 @@ class RedBlackTree {
         x.father = y;
     }
 
-    private void rightRotate(Node x) {
-        final Node y = x.left;
+    private void rightRotate(RedBlackNode x) {
+        final RedBlackNode y = x.left;
         x.left = y.right;
 
         if (y.right != RedBlackTree.nil)
@@ -103,24 +136,24 @@ class RedBlackTree {
 
     public void add(int n) {
         if (this.root == RedBlackTree.nil) {
-            this.root = new Node(n, false);
+            this.root = new RedBlackNode(n, false);
         } else {
-            Node a = this.find(n);
+            RedBlackNode a = this.find(n);
 
             if (n < a.key) {
-                a.left = new Node(n, true);
+                a.left = new RedBlackNode(n, true);
                 a.left.father = a;
                 fixProperties(a.left);
             } else if (n > a.key) {
-                a.right = new Node(n, true);
+                a.right = new RedBlackNode(n, true);
                 a.right.father = a;
                 fixProperties(a.right);
             }
         }
     }
 
-    private void fixProperties(Node z) {
-        Node y;
+    private void fixProperties(RedBlackNode z) {
+        RedBlackNode y;
 
         while (z.father.isRed) {
             if (z.father == z.father.father.left) {
@@ -163,11 +196,11 @@ class RedBlackTree {
         this.root.isRed = false;
     }
 
-    public Node find(int n) {
+    public RedBlackNode find(int n) {
         return this.root.findNode(n);
     }
 
-    private void print(Node node) {
+    private void print(RedBlackNode node) {
         if (node != null && node != nil) {
             System.out.print(node.key + "(" + node.isRed + ") ");
             print(node.left);
@@ -177,5 +210,11 @@ class RedBlackTree {
 
     public void print() {
         print(root);
+    }
+
+    private void updateHeights() {
+        root.updateHeight();
+        root.left.updateHeight();
+        root.right.updateHeight();
     }
 }
