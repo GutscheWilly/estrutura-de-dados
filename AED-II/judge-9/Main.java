b3fdbea3b93af37005c8ca41d58dca29d2feda47
Willy Gutsche <willy.gutsche@unifesp.br>
2023-06-01 20:57:17 -0300

    Close finish

--- AED-II/judge-9/Main.java
+++ AED-II/judge-9/Main.java
@@ -1,39 +1,72 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Hash hash = new Hash(7);
-        hash.add(3);
-        hash.add(4);
-        hash.add(9);
-        hash.add(8);
-        hash.add(1);
-        hash.add(2);
+        final int size = scanner.nextInt();
+        final List<Integer> values = inputValues();
+        final Hash hash = createHash(values, size);
+        final int valueToBeRemoved = scanner.nextInt();
+
+        hash.remove(valueToBeRemoved);
         hash.print();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static List<Integer> inputValues() {
+        final List<Integer> values = new ArrayList<>();
+        int value = scanner.nextInt();
+
+        while (value > -1) {
+            values.add(value);
+            value = scanner.nextInt();
+        }
+        return values;
+    }
+
+    private static Hash createHash(List<Integer> values, int size) {
+        final Hash hash= new Hash(size);
+        values.forEach(hash::add);
+        return hash;
     }
 }
 
 class Hash {
     private final List<Integer> keys;
     private final int size;
+
     private final int emptyValue;
+    private final int removedValue;
 
     public Hash(int size) {
         this.keys = new ArrayList<>(size);
         this.size = size;
         this.emptyValue = -1;
+        this.removedValue = -2;
         initKeys();
     }
 
     private void initKeys() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < this.size; i++) {
             keys.add(emptyValue);
         }
     }
 
     private int hashFunction(int value) {
+        return value % this.size;
+    }
+
+    private int hashFunction(int value, int size) {
         return value % size;
+    }
+
+    private int doubleHashFunction(int value, int collision) {
+        final int h1 = hashFunction(value);
+        final int h2 = 1 + hashFunction(value, this.size - 1);
+        final int balance = h1 + (collision * h2);
+        return hashFunction(balance);
     }
 
     private boolean isKeyNotEmpty(int key) {
@@ -44,31 +77,56 @@ class Hash {
     public void add(int value) {
         int key = hashFunction(value);
 
-        while (isKeyNotEmpty(key)) {
-            key = hashFunction(key + 1);
+        for (int collision = 1; isKeyNotEmpty(key); collision++) {
+            key = doubleHashFunction(value, collision);
         }
-
         keys.set(key, value);
     }
 
-    public boolean contains(int value) {
+    public int contains(int value) {
         int key = hashFunction(value);
 
-        while (isKeyNotEmpty(key)) {
-            int currentValue = keys.get(key);
+        for (int collision = 1; isKeyNotEmpty(key); collision++) {
+            final int currentValue = keys.get(key);
 
             if (currentValue == value) {
-                return true;
+                return key;
             }
 
-            key = hashFunction(key + 1);
+            key = doubleHashFunction(value, collision);
         }
-        return false;
+        return emptyValue;
+    }
+
+    public void remove(int value) {
+        final int key = contains(value);
+
+        if (key == emptyValue) {
+            System.out.println("Valor nao encontrado");
+            return;
+        }
+        keys.set(key, removedValue);
     }
 
     public void print() {
-        for (int key = 0; key < size; key++) {
-            System.out.println(key + " = " + keys.get(key));
+        for (int key = 0; key < this.size - 1; key++) {
+            handlePrint(keys.get(key));
+            System.out.print(" ");
         }
+        handlePrint(keys.get(this.size - 1));
+    }
+
+    private void handlePrint(int value) {
+        if (value == emptyValue) {
+            System.out.print("\\");
+            return;
+        }
+
+        if (value == removedValue) {
+            System.out.print("D");
+            return;
+        }
+
+        System.out.print(value);
     }
 }
