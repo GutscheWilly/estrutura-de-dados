638da6f62c68d2ec68dd02f4b2f81b552292f10f
Willy Gutsche <willy.gutsche@unifesp.br>
2023-07-07 20:54:24 -0300

    Main features

--- AED-II/judge-11/Main.java
+++ AED-II/judge-11/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,21 +17,48 @@ public class Main {
         graph.add(2, 7);
 
         graph.print();
+
+        System.out.println();
+        graph.breadthFirstSearch(0);
     }
 }
 
-class Node {
+enum Color {
+    WHITE,
+    GREY,
+    BLACK
+}
+
+class Node implements Comparable<Node> {
     public Integer value;
     public Node next;
+    public Color color;
 
     public Node(Integer value) {
         this.value = value;
         this.next = null;
+        this.color = Color.WHITE;
     }
 
     public Node(Integer value, Node next) {
         this.value = value;
         this.next = next;
+        this.color = Color.WHITE;
+    }
+
+    public List<Node> getAllTargets() {
+        List<Node> targets = new ArrayList<>();
+
+        for (Node target = next; target != null; target = target.next) {
+            targets.add(target);
+        }
+
+        return targets;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return value - o.value;
     }
 }
 
@@ -66,7 +95,32 @@ class Graph {
             for (Node reference = node; reference != null; reference = reference.next) {
                 System.out.print(reference.value + " -> ");
             }
+
             System.out.println();
         });
+    }
+
+    public void breadthFirstSearch(Integer source) {
+        Node start = nodes.get(source);
+        start.color = Color.GREY;
+
+        List<Node> queue = new ArrayList<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.remove(0);
+
+            for (Node target : node.getAllTargets()) {
+                Node targetNode = nodes.get(target.value);
+
+                if (targetNode.color == Color.WHITE) {
+                    targetNode.color = Color.GREY;
+                    queue.add(targetNode);
+                }
+            }
+
+            node.color = Color.BLACK;
+            System.out.print(node.value + " ");
+        }
     }
 }
